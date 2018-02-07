eaba1b38a3398ba0755ecded04fd91b5e66885d8
Wojciech Krawczyk <wojciech.krawczyk@schibsted.com>
2018-02-07 21:28:45 +0100

    Not null validation

--- adventofcode2016/src/main/java/net/wojtekk/adventofcode2016/day01/Point.java
+++ adventofcode2016/src/main/java/net/wojtekk/adventofcode2016/day01/Point.java
@@ -4,34 +4,36 @@ package net.wojtekk.adventofcode2016.day01;
 import java.util.Objects;
 
 class Point {
-    public int x;
-    public int y;
+  public int x;
+  public int y;
 
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
+  public Point(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
 
-    public Point add(Point other) {
-        return new Point(this.x + other.x, this.y + other.y);
-    }
+  public Point add(Point other) {
+    Objects.requireNonNull(other);
+    return new Point(this.x + other.x, this.y + other.y);
+  }
 
-    public Point multiply(int value) {
-        return new Point(this.x * value, this.y * value);
-    }
+  public Point multiply(int value) {
+    Objects.requireNonNull(value);
+    return new Point(this.x * value, this.y * value);
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (o == this) return true;
-        if (!(o instanceof Point)) {
-            return false;
-        }
-        Point point = (Point) o;
-        return x == point.x && y == point.y;
+  @Override
+  public boolean equals(Object o) {
+    if (o == this) return true;
+    if (!(o instanceof Point)) {
+      return false;
     }
+    Point point = (Point) o;
+    return x == point.x && y == point.y;
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(this.x, this.y);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.x, this.y);
+  }
 }
