e7aadbcd037d3f2d95d5bc5a7368035303a1c21a
Wojciech Krawczyk <wojciech.krawczyk@schibsted.com>
2018-02-14 23:06:20 +0100

    rx playground - part 2

--- rx2/src/main/java/net/wojtekk/rx2/Main.java
+++ rx2/src/main/java/net/wojtekk/rx2/Main.java
@@ -3,6 +3,9 @@ package net.wojtekk.rx2;
 import io.reactivex.Flowable;
 import io.reactivex.schedulers.Schedulers;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
   public static void main(String[] args) {
     System.out.println("\n\nCase 1 - subscribeOn\n");
@@ -58,5 +61,34 @@ public class Main {
       })
       .onErrorResumeNext(Flowable.range(100, 5))
       .blockingSubscribe(System.out::println, error -> System.out.println(error.getMessage()));
+
+    System.out.println("\n\nCase 5 - CompletableFuture inside flatMap\n");
+
+    Flowable.range(1, 10)
+      .flatMap(v -> {
+          CompletableFuture<Integer> future = new CompletableFuture<>();
+          Flowable.timer(1000 - v * 100, TimeUnit.MILLISECONDS)
+            .subscribe(s -> {
+              System.out.println("o");
+              future.complete(v);
+            });
+          System.out.print(".");
+          return Flowable.fromFuture(future);
+        }
+      )
+      .blockingSubscribe(System.out::println);
+
+    System.out.println("\n\nCase 6 - CompletableFuture with subscribeOninside flatMap\n");
+
+    Flowable.range(1, 10)
+      .flatMap(v -> {
+          CompletableFuture<Integer> future = new CompletableFuture<>();
+          Flowable.timer(1000 - v * 100, TimeUnit.MILLISECONDS)
+            .subscribe(s -> future.complete(v));
+          return Flowable.fromFuture(future)
+            .subscribeOn(Schedulers.computation());
+        }
+      )
+      .blockingSubscribe(System.out::println);
   }
 }
