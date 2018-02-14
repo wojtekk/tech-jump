b401f6b0d8edf89180836ad51b957be947226817
Wojciech Krawczyk <wojciech.krawczyk@schibsted.com>
2018-02-14 22:43:58 +0100

    rx playground

---
+++ rx2/src/main/java/net/wojtekk/rx2/Main.java
@@ -0,0 +1,62 @@
+package net.wojtekk.rx2;
+
+import io.reactivex.Flowable;
+import io.reactivex.schedulers.Schedulers;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("\n\nCase 1 - subscribeOn\n");
+
+    Flowable.range(1, 10)
+      .flatMap(v ->
+        Flowable.fromCallable(() -> {
+          Thread.sleep(1000 - v * 100);
+          return v;
+        })
+          .subscribeOn(Schedulers.computation())
+          .map(w -> w * w)
+      )
+      .blockingSubscribe(System.out::println);
+
+    System.out.println("\n\nCase 2 - onErrorReturn inside flatMap\n");
+
+    Flowable.range(1, 10)
+      .flatMap(v ->
+        Flowable.fromCallable(() -> {
+          Thread.sleep(1000 - v * 100);
+          if (v == 4) {
+            throw new Exception("Some error");
+          }
+          return v;
+        })
+          .subscribeOn(Schedulers.computation())
+          .map(w -> w * w)
+          .onErrorReturn(error -> -1)
+      )
+      .blockingSubscribe(System.out::println, error -> System.out.println(error.getMessage()));
+
+    System.out.println("\n\nCase 3 - onErrorReturnItem\n");
+
+    Flowable.range(1, 10)
+      .map(v -> {
+        if (v == 4) {
+          throw new Exception("Some error");
+        }
+        return v;
+      })
+      .onErrorReturnItem(-1)
+      .blockingSubscribe(System.out::println, error -> System.out.println(error.getMessage()));
+
+    System.out.println("\n\nCase 4 - onErrorReturn\n");
+
+    Flowable.range(1, 10)
+      .map(v -> {
+        if (v == 4) {
+          throw new Exception("Some error");
+        }
+        return v;
+      })
+      .onErrorResumeNext(Flowable.range(100, 5))
+      .blockingSubscribe(System.out::println, error -> System.out.println(error.getMessage()));
+  }
+}
